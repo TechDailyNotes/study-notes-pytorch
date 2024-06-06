90079774609999244e0199c08d597a6f12afcf66
ChengaFEI <cf482@cornell.edu>
2024-06-06 05:02:13 -0400

    Refactor tensor creation to include device specification

--- tensor/creation/list-based-creation/demo.py
+++ tensor/creation/list-based-creation/demo.py
@@ -1,4 +1,6 @@
 import torch
 
-x1 = torch.tensor([[1.5, 2.5, 3.5]], dtype=torch.int64)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+x1 = torch.tensor([[1.5, 2.5, 3.5]], dtype=torch.int64, device=device)
 print(x1, x1.dtype, x1.size())
+print(x1.device)

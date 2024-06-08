9c1555db12e3d32f2ffdcfbe9cd9c1b1a12c202c
ChengaFEI <cf482@cornell.edu>
2024-06-08 15:20:44 -0400

    Refactor pixel and sequence data pipeline steps for clarity and consistency

--- techniques/data_engineering/pixel_albumentation/practice.py
+++ techniques/data_engineering/pixel_albumentation/practice.py
@@ -8,22 +8,28 @@ from utils import plot_examples
 
 
 image = Image.open(os.path.join('data/images/elon.jpeg')).convert('RGB')
+mask1 = Image.open(os.path.join('data/images/mask.jpeg')).convert('RGB')
+mask2 = Image.open(os.path.join('data/images/second_mask.jpeg')).convert('RGB')
 transform = A.Compose([
     A.Resize(width=1920, height=1080),
     A.RandomCrop(width=1280, height=720),
     A.Rotate(limit=40, p=0.9, border_mode=cv2.BORDER_CONSTANT),
     A.HorizontalFlip(p=0.5),
-    A.VerticalFlip(p=0.1),
+    A.VerticalFlip(p=0.05),
     A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
     A.OneOf([
         A.Blur(blur_limit=3, p=0.5),
         A.ColorJitter(p=0.5),
-    ], p=1.0),
-])
+    ], p=1.0)
+], is_check_shapes=False)
 
-images_list = [image]
+images = [image]
 image = np.array(image)
-for i in tqdm(range(15)):
-    augmented_image = transform(image=image)['image']
-    images_list.append(augmented_image)
-plot_examples(images_list)
+mask1 = np.array(mask1)
+mask2 = np.array(mask2)
+for _ in tqdm(range(3)):
+    augmented_image_mask = transform(image=image, masks=[mask1, mask2])
+    images.append(augmented_image_mask['image'])
+    images.append(augmented_image_mask['masks'][0])
+    images.append(augmented_image_mask['masks'][1])
+plot_examples(images)
--- techniques/data_engineering/pixel_albumentation/practice_2.py
+++ techniques/data_engineering/pixel_albumentation/practice_2.py
@@ -2,18 +2,19 @@ import albumentations as A
 import cv2
 import numpy as np
 import os
-from PIL import Image
 from tqdm import tqdm
 from utils import plot_examples
 
 
-image = Image.open(os.path.join('data/images/elon.jpeg')).convert('RGB')
+image = cv2.imread(os.path.join('data/images/elon.jpeg'))
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
 transform = A.Compose([
     A.Resize(width=1920, height=1080),
     A.RandomCrop(width=1280, height=720),
     A.Rotate(limit=40, p=0.9, border_mode=cv2.BORDER_CONSTANT),
     A.HorizontalFlip(p=0.5),
-    A.VerticalFlip(p=0.1),
+    A.VerticalFlip(p=0.05),
     A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
     A.OneOf([
         A.Blur(blur_limit=3, p=0.5),
@@ -21,9 +22,10 @@ transform = A.Compose([
     ], p=1.0),
 ])
 
-images_list = [image]
+images = [image]
 image = np.array(image)
-for i in tqdm(range(15)):
-    augmented_image = transform(image=image)['image']
-    images_list.append(augmented_image)
-plot_examples(images_list)
+for _ in tqdm(range(3)):
+    augmentations = transform(image=image)
+    augmented_image = augmentations['image']
+    images.append(augmented_image)
+plot_examples(images)

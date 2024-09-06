c23f39e246a4fb186e4b5cdb71a6f857e64daa19
ChengaFEI <cf482@cornell.edu>
2024-09-06 14:55:35 -0700

    Add

--- components/distributed_training/cudautils.py
+++ components/distributed_training/cudautils.py
@@ -1,4 +1,7 @@
 import torch
 
+# torch.cuda.set_device(0)
+
 print(f"torch.cuda.device_count() = {torch.cuda.device_count()}")
 print(f"torch.cuda.is_available() = {torch.cuda.is_available()}")
+print(f"torch.version.cuda = {torch.version.cuda}")
--- components/distributed_training/main.py
+++ components/distributed_training/main.py
@@ -42,8 +42,7 @@ class Trainer:
 
     def _run_epoch(self, epoch):
         b_sz = len(next(iter(self.train_data))[0])
-        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {
-              b_sz} | Steps: {len(self.train_data)}")
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
         for source, targets in self.train_data:
             source = source.to(self.gpu_id)
             targets = targets.to(self.gpu_id)

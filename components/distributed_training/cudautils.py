09201f6c8cb72b584c53c5df8ff9ce5df3d5f37c
ChengaFEI <cf482@cornell.edu>
2024-09-06 15:12:56 -0700

    Add

--- components/distributed_training/cudautils.py
+++ components/distributed_training/cudautils.py
@@ -1,7 +1,5 @@
 import torch
 
-# torch.cuda.set_device(0)
-
 print(f"torch.cuda.device_count() = {torch.cuda.device_count()}")
 print(f"torch.cuda.is_available() = {torch.cuda.is_available()}")
 print(f"torch.version.cuda = {torch.version.cuda}")
